dfc6c97e3d6b52a061c440036bc62452c32bfb3e
ZidhanArrasyid <arrasyidzidhan@gmail.com>
2023-12-04 21:02:13 +0700

    firsh progres

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,27 +3,33 @@ import { getMovies } from "../../api";
 import styles from "./style.module.css";
 
 const Search = ({ setMovies, setLoading }) => {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState("");
+  const handleClick = (keyword) => {
+    setLoading(true);
+    getMovies(keyword).then((res) => {
+      if (res.data.Response === "False") {
+        setMovies(res.data.Error);
+      } else {
+        setMovies(res.data.Search);
+      }
+      setLoading(false);
+    });
+  };
 
-    const handleClick = (keyword) => {
-        setLoading(true);
-        getMovies(keyword).then(res => {
-            if (res.data.Response === "False") {
-                setMovies(res.data.Error);
-            } else {
-                setMovies(res.data.Search);
-            }
-            setLoading(false);
-        });
-    };
-
-    return (
-        <div className={styles.container}>
-            <input value={input} placeholder="Search Movie Title" type="search" onChange={(e) => setInput(e.target.value)} />
-            <button type="button" onClick={() => handleClick(input)}>Search</button>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <input
+        value={input}
+        placeholder="Search Movie Title"
+        type="search"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="button" onClick={() => handleClick(input)}>
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default Search;
