a41184154e95e5c4a81591430fc9e68d2913ea0d
bimajr <abimajr11@gmail.com>
2023-12-08 19:31:50 +0800

    repair and update pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import styles from "./style.module.css";
 
 const Footer = () => {
-    return (
-        <footer className={styles.container}>
-            <small>&copy; 2022 | Kelompok 08 MovieFinder</small>
-        </footer>
-    );
+  return (
+    <footer className={styles.container}>
+      <small>&copy; 2023 | Kelompok 08 Movie8</small>
+    </footer>
+  );
 };
 
 export default Footer;
