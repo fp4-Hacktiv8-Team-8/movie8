f9ee9d168e6cfe40804970c1d19086348016c761
Farhanmuh69 <fmuh748@gmail.com>
2023-12-08 18:16:08 +0700

    update detail footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./style.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.container}>
+            <small>&copy; 2022 | Kelompok 08 MovieFinder</small>
+        </footer>
+    );
+};
+
+export default Footer;
